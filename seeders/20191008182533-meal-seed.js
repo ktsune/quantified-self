bf57a1a65464feb005d03b19069cce93e2148b44
Sarah Tatro <statrox4@gmail.com>
2019-10-09 12:20:38 -0600

    Create meal seed

---
+++ seeders/20191008182533-meal-seed.js
@@ -0,0 +1,26 @@
+'use strict';
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+
+        return queryInterface.bulkInsert('Meals', [{
+          name: 'breakfast',
+          createdAt: new Date(),
+          updatedAt: new Date()
+        },
+        {
+          name: 'lunch',
+          createdAt: new Date(),
+          updatedAt: new Date()
+        },
+        {
+          name: 'dinner',
+          createdAt: new Date(),
+          updatedAt: new Date()
+        }
+        ], {});
+      },
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.bulkDelete('Meals', null, {});
+  }
+};
