e7db9c809ac7f4048f0376f5e452fb0e69f90584
Sarah Tatro <statrox4@gmail.com>
2019-10-09 12:20:02 -0600

    Create foods model

--- models/food.js
+++ models/food.js
@@ -1,11 +1,18 @@
 'use strict';
 module.exports = (sequelize, DataTypes) => {
+
   const Food = sequelize.define('Food', {
     name: DataTypes.STRING,
     calories: DataTypes.INTEGER
   }, {});
   Food.associate = function(models) {
     // associations can be defined here
+    Food.belongsToMany(models.Meal, {
+    through: 'MealFoods',
+    as: 'meals',
+    foreignKey: 'foodId',
+    otherKey: 'mealId'
+    });
   };
   return Food;
 };
