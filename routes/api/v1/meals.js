fdf753a8df1c08bff064bf404fd0054b13aee84b
Sarah Tatro <statrox4@gmail.com>
2019-10-09 15:53:15 -0600

    Add include to meals route to pass food to meal

--- routes/api/v1/meals.js
+++ routes/api/v1/meals.js
@@ -2,22 +2,24 @@ var express = require('express');
 var router = express.Router();
 var fetch = require("node-fetch");
 var Meal = require("../../../models").Meal
+var Food = require("../../../models").Food
 
 /* GET all meals */
 router.get("/", function(req, res, next) {
   res.setHeader("Content-Type", "application/json");
-  Meal.findAll({
-    // where: {
-    //   id: foodId
-    // }
-  })
-    .then(meals => {
-      console.log('MEALS', meals)
-      res.status(200).send(JSON.stringify(meals));
+    Meal.findAll({
+      where: {
+        attributes: [id, name],
+        include: 'foods'
+      }
     })
-    .catch(error => {
-      res.status(404).send({error})
-    });
+  .then(meals => {
+    console.log('MEALS', meals)
+    res.status(200).send(JSON.stringify(meals));
+  })
+  .catch(error => {
+    res.status(404).send({error})
+  });
 });
 
 module.exports = router;
